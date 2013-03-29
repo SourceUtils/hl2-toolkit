5711acc88ae05843c8034e52b0b1b0fe59117d5c
TimePath <andrew.hardaker1995@gmail.com>
2013-03-29 18:22:57 +1100

    VTX loading

--- hl2/com/timepath/hl2/io/MDL.java
+++ hl2/com/timepath/hl2/io/MDL.java
@@ -15,32 +15,53 @@ import java.util.logging.Logger;
 /**
  * Studiomodel
  *
+ * https://github.com/toji/webgl-source/tree/master/js
+ * https://github.com/toji/webgl-source/blob/master/js/source-mdl-struct.js
+ * https://github.com/toji/webgl-source/blob/master/js/source-mdl.js
+ *
  * @author timepath
  */
 public class MDL {
 
+    public static void main(String... args) throws IOException {
+        MDL.load("/home/timepath/NetBeansProjects/TF2 Map Loader/src/tf/models/weapons/v_models/v_sapper_spy");
+    }
+    
     private static final Logger LOG = Logger.getLogger(MDL.class.getName());
+    private static final int MAX_NUM_LODS = 8;
+    private static final int MAX_NUM_BONES_PER_VERT = 3;
+    private static final int MAX_NUM_BONES_PER_TRI = 9;
+    private static final int VERTEX_STRIDE = 64;
     private VVD vvd;
 
-    public float[] getverts() {
+    public float[] getVertices() {
         return vvd.verts;
     }
 
-    public float[] getnorm() {
+    public float[] getNormals() {
         return vvd.norm;
     }
 
-    public float[] gettangents() {
+    public float[] getTangents() {
         return vvd.tangents;
     }
 
-    public float[] getuv() {
+    public float[] getTextureCoordinates() {
         return vvd.uv;
     }
     private VTX vtx;
 
+    public int[] getIndices() {
+        int[] b = new int[vtx.indices.size()];
+        for(int i = 0; i < vtx.indices.size(); i++) {
+            Long l = vtx.indices.get(i);
+            b[i] = l.intValue();
+        }
+        return b;
+    }
+
     public static MDL load(String fileName) throws IOException {
-        LOG.info(fileName);
+        LOG.log(Level.INFO, "\n\nLoading MDL {0}", fileName);
         MDL mdl = MDL.load(new File(fileName + ".mdl"));
         mdl.vvd = VVD.load(new File(fileName + ".vvd"));
         mdl.vtx = VTX.load(new File(fileName + ".sw.vtx"));
@@ -72,11 +93,13 @@ public class MDL {
     private static class VTX {
 
         private static final Logger LOG = Logger.getLogger(VTX.class.getName());
+        private ArrayList<Long> indices = new ArrayList<Long>();
 
         private VTX() {
         }
 
         private static VTX load(File file) throws IOException {
+            LOG.log(Level.INFO, "\n\nLoading VTX {0}", file);
             MappedByteBuffer mbb = new FileInputStream(file).getChannel().map(FileChannel.MapMode.READ_ONLY, 0, file.length());
             mbb.order(ByteOrder.LITTLE_ENDIAN);
             return load(mbb);
@@ -84,87 +107,103 @@ public class MDL {
 
         private static VTX load(ByteBuffer buf) {
             VTX vtx = new VTX();
+            //<editor-fold defaultstate="collapsed" desc="VTX Header">
             // file version as defined by OPTIMIZED_MODEL_FILE_VERSION (currently 7)
             int version = buf.getInt(); // 0
-
             // hardware params that affect how the model is to be optimized.
             int vertCacheSize = buf.getInt(); // 4
             long maxBonesPerStrip = buf.getShort(); // 8
-            long maxBonesPerFace = buf.getShort(); // 10
+            long maxBonesPerTri = buf.getShort(); // 10
             int maxBonesPerVert = buf.getInt(); // 12
-
             // must match checkSum in the .mdl
-            long checkSum = buf.getLong(); // 16
-
-            int lodCount = buf.getInt(); // 24 // garymcthack - this is also specified in ModelHeader_t and should match
-
+            long checkSum = buf.getInt(); // 16
+            int numLODs = buf.getInt(); // 20 // garymcthack - this is also specified in ModelHeader_t and should match
             // this is an offset to an array of 8 MaterialReplacementListHeader_t's, one of these for each LOD
-            int materialReplacementListOffset = buf.getInt(); // 28
+            int materialReplacementListOffset = buf.getInt(); // 24
+            int numBodyParts = buf.getInt(); // 28
+            int bodyPartOffset = buf.getInt(); // 32 // offset to an array of BodyPartHeader_t's
+            //</editor-fold>
+            LOG.log(Level.INFO, "\t\t\tver:{0}, vertCache:{1}, bones/strip:{2}, bones/tri:{3}, bones/vert:{4}, check:{5}, lods:{6}, replOff:{7}, parts:{8}, partOff:{9}", new Object[] {version, vertCacheSize, maxBonesPerStrip, maxBonesPerTri, maxBonesPerVert, checkSum, numLODs, materialReplacementListOffset, numBodyParts, bodyPartOffset});
 
-            int numBodyParts = buf.getInt(); // 32
-            int bodyPartOffset = buf.getInt(); // 36 // offset to an array of BodyPartHeader_t's
 
-            // 40
-            LOG.log(Level.INFO, "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}", new Object[] {version, vertCacheSize, maxBonesPerStrip, maxBonesPerFace, maxBonesPerVert, checkSum, lodCount, materialReplacementListOffset, numBodyParts, bodyPartOffset});
+            LOG.log(Level.INFO, "parts[] = {2}: {0} vs {1}", new Object[] {buf.position(), bodyPartOffset, numBodyParts});
+            buf.position(bodyPartOffset);
+            for(int part = 0; part < numBodyParts; part++) {
+                int part_numModels = buf.getInt();
+                int part_modelOffset = bodyPartOffset + buf.getInt();
 
-            long vertCount = buf.getInt(); // 40
-            long vertTableOffset = buf.getInt(); // 44
-            long vertFaceCount = buf.getInt(); // 48
-            long vertFaceTableOffset = buf.getInt(); // 52
-            long meshCount = buf.getInt(); // 56
-            long meshTableOffset = 52 + buf.getInt(); // 60
-            byte unknown = buf.get(); // 64
-            // 65
+                LOG.log(Level.INFO, "parts[{3}].models[] = {2}: {0} vs {1}", new Object[] {buf.position(), part_modelOffset, part_numModels, part});
+                buf.position(part_modelOffset);
+                for(int model = 0; model < part_numModels; model++) {
+                    int mdl_numLODs = buf.getInt();
+                    int mdl_lodOffset = part_modelOffset + buf.getInt();
 
-            long pos1 = meshTableOffset;
-            for(int l = 0; l < lodCount; l++) {
-                ArrayList<Long> smesh = new ArrayList<Long>();
-                for(int n = 0; n < meshCount; n++) {
-                    smesh.add(pos1);
-                    buf.position((int) pos1);
-                    smesh.add((long) buf.getInt());
-                    pos1 += 4;
-                    buf.position((int) pos1);
-                    smesh.add((long) buf.getInt());
-                    pos1 += 5;
-                }
-                long pos2 = 0;
-                ArrayList<Long> faces = new ArrayList<Long>();
-                for(int m = 0; m < meshCount * 3; m += 3) {
-                    long offset = smesh.get(m) + smesh.get(m + 2);
-                    long parts = smesh.get(m + 1);
-                    for(int y = 0; y < parts; y++) {
-                        ArrayList<Long> vert_data = new ArrayList<Long>();
-                        pos2 = offset;
-                        faces.add(parts);
-                        buf.position((int) pos2);
-                        long vert_num = buf.getInt();
-                        faces.add(vert_num);
-                        pos2 += 4;
-                        buf.position((int) pos2);
-                        long vert_pos = offset + buf.getInt();
-                        long vert_l = vert_pos + (vert_num * 9);
-                        while(vert_l > vert_pos) {//[byte] ID of vert from VVD binary.
-                            vert_pos += 4;
-                            buf.position((int) vert_pos);
-                            vert_data.add((long) buf.getShort());
-                            vert_pos += 5;
+                    LOG.log(Level.INFO, "parts[{4}].models[{3}].lods[] = {2}: {0} vs {1}", new Object[] {buf.position(), mdl_lodOffset, mdl_numLODs, model, part});
+                    buf.position(mdl_lodOffset);
+                    for(int lod = 0; lod < mdl_numLODs; lod++) {
+                        int lod_numMeshes = buf.getInt();
+                        int lod_meshOffset = mdl_lodOffset + buf.getInt();
+                        float lod_switchPoint = buf.getFloat();
+
+                        LOG.log(Level.INFO, "parts[{5}].model[{4}].lod[{3}].meshes[] = {2}: {0} vs {1}", new Object[] {buf.position(), lod_meshOffset, lod_numMeshes, lod, model, part});
+                        buf.position(lod_meshOffset);
+                        for(int mesh = 0; mesh < lod_numMeshes; mesh++) {
+                            int mesh_numStripGroups = buf.getInt();
+                            int mesh_stripGroupHeaderOffset = lod_meshOffset + buf.getInt();
+                            short mesh_flags = buf.get();
+
+                            LOG.log(Level.INFO, "parts[{6}].model[{5}].lod[{4}].meshes[{3}].stripGroups[] = {2}: {0} vs {1}", new Object[] {buf.position(), mesh_stripGroupHeaderOffset, mesh_numStripGroups, mesh, lod, model, part});
+                            buf.position(mesh_stripGroupHeaderOffset);
+                            for(int group = 0; group < mesh_numStripGroups; group++) {
+                                int group_numVerts = buf.getInt();
+                                int group_vertOffset = mesh_stripGroupHeaderOffset + buf.getInt();
+                                int group_numIndices = buf.getInt();
+                                int group_indexOffset = mesh_stripGroupHeaderOffset + buf.getInt();
+                                int group_numStrips = buf.getInt();
+                                int group_stripOffset = mesh_stripGroupHeaderOffset + buf.getInt();
+                                short group_flags = buf.get();
+
+                                LOG.log(Level.INFO, "\t\t\tstripOff: {2}, vertOff: {0}, indOff: {1},", new Object[] {group_vertOffset, group_indexOffset, group_stripOffset});
+
+                                group_stripOffset = 0; // don't care
+                                group_vertOffset = 0; // don't care
+
+                                LOG.log(Level.INFO, "parts[{7}].model[{6}].lod[{5}].meshes[{4}].stripGroups[{3}].strips[] = {2}: {0} vs {1}", new Object[] {buf.position(), group_stripOffset, group_numStrips, group, mesh, lod, model, part});
+                                buf.position(group_stripOffset);
+                                for(int strip = 0; strip < group_numStrips; strip++) {
+                                    int strip_numIndices = buf.getInt();
+                                    int strip_indexOffset = buf.getInt();
+                                    int strip_numVerts = buf.getInt();
+                                    int strip_vertOffset = buf.getInt();
+                                    short strip_numBones = buf.getShort();
+                                    short strip_flags = buf.get();
+                                    int strip_numBoneStateChanges = buf.getInt();
+                                    int strip_boneStateChangeOffset = buf.getInt();
+                                }
+
+                                LOG.log(Level.INFO, "parts[{7}].model[{6}].lod[{5}].meshes[{4}].stripGroups[{3}].verts[] = {2}: {0} vs {1}", new Object[] {buf.position(), group_vertOffset, group_numVerts, group, mesh, lod, model, part});
+                                buf.position(group_vertOffset);
+                                for(int vert = 0; vert < group_numVerts; vert++) {
+                                    short[] vert_boneWeightIndex = new short[MAX_NUM_BONES_PER_VERT];
+                                    for(int bw = 0; bw < MAX_NUM_BONES_PER_VERT; bw++) {
+                                        vert_boneWeightIndex[bw] = buf.get();
+                                    }
+                                    short vert_numBones = buf.get();
+                                    int vert_origMeshVertID = buf.getShort();
+                                    short[] vert_boneID = new short[MAX_NUM_BONES_PER_VERT];
+                                    for(int bi = 0; bi < MAX_NUM_BONES_PER_VERT; bi++) {
+                                        vert_boneID[bi] = buf.get();
+                                    }
+                                }
+
+                                LOG.log(Level.INFO, "parts[{7}].model[{6}].lod[{5}].meshes[{4}].stripGroups[{3}].indices[] = {2}: {0} vs {1}", new Object[] {buf.position(), group_indexOffset, group_numIndices, group, mesh, lod, model, part});
+                                buf.position(group_indexOffset);
+                                short[] indices = new short[group_numIndices];
+                                for(int index = 0; index < group_numIndices; index++) {
+                                    indices[index] = buf.getShort();
+                                }
+                            }
                         }
-                        pos2 += +4;
-                        buf.position((int) pos2);
-                        long tri_num = buf.getInt();
-                        pos2 += +4;
-                        buf.position((int) pos2);
-                        long tri_pos = offset + buf.getInt();
-                        long tri_l = tri_num * 2;
-//                        long vert_seq = buf.sub($tri_pos, $tri_l); //[byte] Vert sequence to draw faces in correct order.
-//                        foreach($vert_seq as $vid) //Arrange vert IDs in correct order to be outputted to SMD.
-//                        {
-//                                array_push($faces, $vert_data[$vid]);
-//                        }
-//                        m_data.add(faces);
-//                        faces = array();
-                        offset += 25;
                     }
                 }
             }
@@ -182,8 +221,6 @@ public class MDL {
      */
     private static class VVD {
 
-        private static final int MAX_NUM_LODS = 8;
-        private static final int MAX_NUM_BONES_PER_VERT = 3;
         public float[] verts, norm, uv, tangents;
         private static final Logger LOG = Logger.getLogger(VVD.class.getName());
 
@@ -191,6 +228,7 @@ public class MDL {
         }
 
         public static VVD load(File file) throws IOException {
+            LOG.log(Level.INFO, "\n\nLoading VVD {0}", file);
             MappedByteBuffer mbb = new FileInputStream(file).getChannel().map(FileChannel.MapMode.READ_ONLY, 0, file.length());
             mbb.order(ByteOrder.LITTLE_ENDIAN);
             return load(mbb);
@@ -201,8 +239,8 @@ public class MDL {
             // Header
             int id = buf.getInt();                        // MODEL_VERTEX_FILE_ID
             int version = buf.getInt();                   // MODEL_VERTEX_FILE_VERSION
-            long checksum = buf.getLong();                // same as studiohdr_t, ensures sync
-//            int numLODs = buf.getInt();                     // num of valid lods
+            long checksum = buf.getInt();                 // same as studiohdr_t, ensures sync
+            int numLODs = buf.getInt();                   // num of valid lods
             int[] numLODVertexes = new int[MAX_NUM_LODS]; // num verts for desired root lod
             for(int l = 0; l < numLODVertexes.length; l++) {
                 numLODVertexes[l] = buf.getInt();
