e57395c4beb8dca502833f89e94bc0e78084d941
TimePath <andrew.hardaker1995@gmail.com>
2013-04-11 21:25:40 +1000

    Fixes for my old powermac G5

--- plaf/com/timepath/plaf/OS.java
+++ plaf/com/timepath/plaf/OS.java
@@ -13,11 +13,11 @@ import java.util.logging.Logger;
 public enum OS {
 
     Windows, Mac, Linux, Other;
-    
+
     private static final Logger LOG = Logger.getLogger(OS.class.getName());
-    
+
     private static OS system;
-    
+
     static {
         //<editor-fold defaultstate="collapsed" desc="OS detection">
         String osVer = System.getProperty("os.name").toLowerCase();
@@ -29,51 +29,50 @@ public enum OS {
             system = OS.Linux;
         } else {
             system = OS.Other;
-            LOG.log(Level.WARNING, "Unrecognised OS: {0}", osVer);
+            LOG.log(Level.INFO, "OS string: {0}", osVer);
         }
+        LOG.log(Level.INFO, "OS: {0}", system);
         //</editor-fold>
     }
-    
+
     public static OS get() {
         return system;
     }
-    
+
     public static boolean isWindows() {
         return system == Windows;
     }
-    
+
     public static boolean isMac() {
         return system == Mac;
     }
-    
+
     public static boolean isLinux() {
         return system == Linux;
     }
-    
+
     public static class WindowToolkit {
 
         private static String windowClass;
-        
+
         public static String getWindowClass() {
             return WindowToolkit.windowClass;
         }
-        
+
         /**
          * Doesn't seem to work all the time (under Linux)
-         * @param windowClass 
+         *
+         * @param windowClass
          */
         public static void setWindowClass(String windowClass) {
             WindowToolkit.windowClass = windowClass;
-            
+
             if(OS.isLinux()) {
                 System.setProperty("jayatana.startupWMClass", windowClass);
 //                boolean force = "Unity".equals(System.getenv("XDG_CURRENT_DESKTOP")); // UBUNTU_MENUPROXY=libappmenu.so
 //                if(force) {
 //                    System.setProperty("jayatana.force", "true");
 //                }
-            }
-            
-            if(!OS.isWindows()) {
                 try {
                     Toolkit xToolkit = Toolkit.getDefaultToolkit();
                     Field awtAppClassNameField = xToolkit.getClass().getDeclaredField("awtAppClassName");
@@ -82,16 +81,15 @@ public enum OS {
                 } catch(Exception ex) {
                     LOG.log(Level.WARNING, null, ex);
                 }
+
+                DesktopLauncher.create(windowClass, "/com/timepath/tf2/hudeditor/resources",
+                                       new String[]{"Icon.png", "Icon.svg"},
+                                       new String[]{windowClass, windowClass});
             }
-            
-            DesktopLauncher.create(windowClass, "/com/timepath/tf2/hudeditor/resources",
-                                   new String[]{"Icon.png", "Icon.svg"},
-                                   new String[]{windowClass, windowClass});
         }
 
         private WindowToolkit() {
         }
-    
     }
-    
+
 }
--- plaf/com/timepath/plaf/x/filechooser/FileChooserTest.java
+++ plaf/com/timepath/plaf/x/filechooser/FileChooserTest.java
@@ -52,7 +52,7 @@ public class FileChooserTest extends javax.swing.JFrame {
         checkSave = new javax.swing.JCheckBox();
         checkMulti = new javax.swing.JCheckBox();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("File dialog tester");
 
         jList1.setModel(getFileChoosers());
@@ -290,37 +290,6 @@ public class FileChooserTest extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String... args) {
-        /*
-         * Set the Nimbus look and feel
-         */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /*
-         * If Nimbus (introduced in Java SE 6) is not available, stay with the default look and
-         * feel.
-         * For details see
-         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for(javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch(ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FileChooserTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch(InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FileChooserTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch(IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FileChooserTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch(javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FileChooserTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /*
-         * Create and display the form
-         */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new FileChooserTest().setVisible(true);
