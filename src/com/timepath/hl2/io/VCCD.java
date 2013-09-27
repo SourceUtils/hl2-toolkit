40bc3fc26d16e9e405abadff768d179534e81ca3
TimePath <andrew.hardaker1995@gmail.com>
2013-09-27 19:57:38 +1000

    Updated captions to use a ByteBuffer implementation and added a test case

--- src/com/timepath/hl2/io/VCCD.java
+++ src/com/timepath/hl2/io/VCCD.java
@@ -1,17 +1,15 @@
 package com.timepath.hl2.io;
 
-import com.timepath.io.RandomAccessFileWrapper;
 import com.timepath.steam.io.util.Property;
 import com.timepath.steam.io.VDF;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.CRC32;
@@ -22,181 +20,152 @@ import java.util.zip.CRC32;
  * https://developer.valvesoftware.com/wiki/Subtitles_(Portal_2)
  * https://developer.valvesoftware.com/wiki/Soundscript
  * phonemes
- *
- * @author timepath
+ * 
+ * @author TimePath
  */
 public class VCCD {
-
+    
     private static final Logger LOG = Logger.getLogger(VCCD.class.getName());
 
-    public VCCD() {
+    private VCCD() {
     }
 
-    /**
-     * Used for writing captions
-     *
-     * @param curr
-     * @param round
-     *
-     * @return
-     */
-    private static int alignValue(double curr, double round) {
-        return (int) (Math.ceil(curr / round) * round);
+    private static int HEADER_MAGIC = (('V') | ('C' << 8) | ('C' << 16) | ('D' << 24));
+
+    private static int HEADER_VERSION_EXPECTED = 1;
+    
+    private static final int HEADER_SIZE = (4 * 6);
+
+    private static final int ENTRY_SIZE = (4 * 2) + (2 * 2);
+
+    private static final int DEFAULT_BLOCK_SIZE = 8192;
+
+    private static final Charset VALUE_CHARSET = Charset.forName("UTF-16LE");
+
+    public static List<CaptionEntry> load(InputStream is) throws IOException {
+        byte[] array = new byte[is.available()];
+        is.read(array);
+        is.close();
+        ByteBuffer buf = ByteBuffer.wrap(array);
+        return load(buf);
     }
 
-    private static int expectedHeader = (('V') | ('C' << 8) | ('C' << 16) | ('D' << 24));
+    public static List<CaptionEntry> load(ByteBuffer buf) {
+        LOG.log(Level.INFO, "Loading from {0}", buf);
+        buf.order(ByteOrder.LITTLE_ENDIAN);
 
-    private static int expectedVersion = 1;
-
-    public static ArrayList<CaptionEntry> load(InputStream is) {
-        ArrayList<CaptionEntry> list = new ArrayList<CaptionEntry>();
-        try {
-            byte[] array = new byte[is.available()];
-            is.read(array);
-            ByteBuffer buf = ByteBuffer.wrap(array);
-            buf.order(ByteOrder.LITTLE_ENDIAN);
-
-            if(buf.getInt() != expectedHeader) {
-                LOG.severe("Header mismatch");
-            }
-            int version = buf.getInt();
-            if(version != expectedVersion) {
-                LOG.log(Level.WARNING, "Unsupported version: {0}", version);
-            }
-            int blocks = buf.getInt();
-            int blockSize = buf.getInt();
-            int directorySize = buf.getInt();
-            int dataOffset = buf.getInt();
-            LOG.log(Level.INFO,
-                    "Version: {0}, Blocks: {1}, BlockSize: {2}, DirectorySize: {3}, DataOffset: {4}",
-                    new Object[] {version, blocks, blockSize, directorySize, dataOffset});
-
-            CaptionEntry[] entries = new CaptionEntry[directorySize];
-            for(int i = 0; i < directorySize; i++) {
-                CaptionEntry e = new CaptionEntry();
-                e.setKey(buf.getInt());
-                e.setBlock(buf.getInt());
-                e.setOffset(buf.getShort());
-                e.setLength(buf.getShort());
-//                    System.out.println("<" + i + " - " + e);
-                entries[i] = e;
-            }
-            buf.position(dataOffset);
-            for(int i = 0; i < directorySize; i++) {
-                buf.position(dataOffset + (entries[i].block * blockSize) + entries[i].offset);
-                StringBuilder sb = new StringBuilder((entries[i].length / 2) - 1);
-                for(int x = 0; x < (entries[i].length / 2) - 1; x++) {
-                    sb.append(buf.getChar()); // 2 bytes
-                }
-                buf.get(new byte[2]);
-                entries[i].setValue(sb.toString());
-                list.add(entries[i]);
-            }
-            is.close(); // The rest of the file is garbage, 0's or otherwise
-        } catch(IOException ex) {
-            LOG.log(Level.SEVERE, null, ex);
+        if(buf.getInt() != HEADER_MAGIC) {
+            LOG.severe("Header mismatch");
         }
-        return list;
+        int version = buf.getInt();
+        if(version != HEADER_VERSION_EXPECTED) {
+            LOG.log(Level.WARNING, "Unsupported version: {0}", version);
+        }
+        int blocks = buf.getInt();
+        int blockSize = buf.getInt();
+        int totalEntries = buf.getInt();
+        int dataOffset = buf.getInt();
+        LOG.log(Level.FINE,
+                "Version: {0}, Blocks: {1}, BlockSize: {2}, DirectorySize: {3}, DataOffset: {4}",
+                new Object[] {version, blocks, blockSize, totalEntries, dataOffset});
+
+        CaptionEntry[] entries = new CaptionEntry[totalEntries];
+        for(int i = 0; i < entries.length; i++) {
+            CaptionEntry e = new CaptionEntry();
+            e.setKey(buf.getInt());
+            e.setBlock(buf.getInt());
+            e.setOffset(buf.getShort());
+            e.setLength(buf.getShort());
+            entries[i] = e;
+            LOG.log(Level.FINE, "Loading {0}, {1} ({2}->{3})", new Object[] {
+                i, e.getKey(), e.getOffset(), e.getOffset() + e.getLength()});
+        }
+        buf.position(dataOffset);
+        for(CaptionEntry e : entries) {
+            buf.position(dataOffset + (e.block * blockSize) + e.offset);
+            int limit = buf.limit();
+            int size = e.length - 2;
+            buf.limit(buf.position() + size);
+            e.setValue(VALUE_CHARSET.decode(buf.slice()).toString());
+            buf.limit(limit);
+        }
+        // The rest of the file is useless, 0's or otherwise
+        LOG.log(Level.INFO, "Loaded from {0}", buf);
+        return Arrays.asList(entries);
     }
 
-    /**
-     * Ensure alphabetical order
-     *
-     * @param file
-     * @param entries
-     */
-    public static void save(String file, ArrayList<CaptionEntry> entries) {
-        if(file == null) {
-            return;
-        }
-        try {
-            Collections.sort(entries);
-            int directorySize = entries.size();
-            int blockSize = 8192;
-            int length = 0;
-            int blocks = 1;
-            for(int i = 0; i < directorySize; i++) {
-                int eval = length + entries.get(i).getLength();
-                if(eval > blockSize) {
-                    blocks++;
-                    length = entries.get(i).getLength();
-                } else {
-                    length = eval;
-                }
-            }
+    public static ByteBuffer save(List<CaptionEntry> entries, OutputStream os) throws IOException {
+        ByteBuffer buf = save(entries);
+        byte[] bytes = new byte[buf.limit()];
+        buf.get(bytes);
+        buf.flip();
+        os.write(bytes);
+        os.flush();
+        os.close();
+        return buf;
+    }
 
-            LOG.log(Level.FINE, "Blocks: {0}", blocks);
+    public static ByteBuffer save(List<CaptionEntry> entries) throws UnsupportedEncodingException {
+        Collections.sort(entries); // Ensure alphabetical order
 
-            int dataOffset = alignValue((6 * 4) + (directorySize * 12), 512);
-
-            File f = new File(file);
-            if(f.exists()) {
-                f.delete();
-            } else {
-                f.createNewFile();
-            }
-            RandomAccessFileWrapper rf = new RandomAccessFileWrapper(f, "rw");
-            rf.writeLEInt(expectedHeader);
-            rf.writeLEInt(1);
-            rf.writeLEInt(blocks);
-            rf.writeLEInt(blockSize);
-            rf.writeLEInt(directorySize);
-            rf.writeLEInt(dataOffset);
-
-            int currentBlock = 0;
-            int firstInBlock = 0;
-            for(int i = 0; i < directorySize; i++) {
-                CaptionEntry e = entries.get(i);
-                e.setBlock(0);
+        // Pack into blocks
+        int blockSize = DEFAULT_BLOCK_SIZE;
+        int totalLength = 0, requiredBlocks = 1, blockPtr;
+        for(CaptionEntry e : entries) {
+            blockPtr = totalLength % blockSize;
+            if(blockPtr + e.getLength() > blockSize) {
                 e.setOffset(0);
-
-                int offset;
-
-                int proposedOffset = 0;
-                for(int j = firstInBlock; j < i; j++) {
-                    proposedOffset += entries.get(j).getLength();
-                }
-                if((proposedOffset + e.getLength()) > blockSize) {
-                    offset = 0;
-                    currentBlock++;
-                    firstInBlock = i;
-                    LOG.log(Level.FINER, "Doesn't fit; new block");
-                } else {
-                    offset = proposedOffset;
-                }
-
-                e.setBlock(currentBlock);
-                e.setOffset(offset);
-
-//                    System.out.println(">" + i + " - " + e);
-
-                rf.writeULong(e.getKey());
-                rf.writeLEInt(e.getBlock());
-                rf.writeULEShort((short) e.getOffset());
-                rf.writeULEShort((short) e.getLength());
+                totalLength = (requiredBlocks - 1) * blockSize;
+                requiredBlocks++;
+            } else {
+                e.setOffset(blockPtr);
             }
-
-            rf.write(new byte[(dataOffset - (int) rf.getFilePointer())]);
-
-            int lastBlock = 0;
-            for(int i = 0; i < directorySize; i++) {
-                CaptionEntry e = entries.get(i);
-                if(e.getBlock() > lastBlock) {
-                    lastBlock = e.getBlock();
-                    rf.write(new byte[blockSize - (entries.get(i - 1).getOffset() + entries.get(
-                                                   i - 1).getLength())]);
-                }
-                rf.writeLEChars(e.getValue());
-                rf.writeLEChar(0);
-            }
-            int last = entries.size() - 1;
-            rf.write(
-                    new byte[blockSize - (entries.get(last).getOffset() + entries.get(last).getLength())]);
-            rf.close();
-        } catch(IOException ex) {
-            LOG.log(Level.SEVERE, null, ex);
+            e.setBlock(requiredBlocks - 1);
+            totalLength += e.getLength();
         }
-        LOG.log(Level.INFO, "Saved {0}", file);
+
+        int dataOffset = (HEADER_SIZE + (entries.size() * ENTRY_SIZE));
+        // Round up to nearest multiple of 512
+        int multiple = 512;
+        dataOffset = ((dataOffset + multiple - 1) / multiple) * multiple;
+        int totalSize = dataOffset + (requiredBlocks * blockSize);
+
+        ByteBuffer buf = ByteBuffer.allocate(totalSize);
+        buf.order(ByteOrder.LITTLE_ENDIAN);
+        LOG.log(Level.INFO, "Saving to {0}", buf);
+        buf.putInt(HEADER_MAGIC);
+        int version = 1;
+        buf.putInt(version);
+        buf.putInt(requiredBlocks);
+        buf.putInt(blockSize);
+        buf.putInt(entries.size());
+        buf.putInt(dataOffset);
+
+        LOG.log(Level.FINE,
+                "Version: {0}, Blocks: {1}, BlockSize: {2}, DirectorySize: {3}, DataOffset: {4}",
+                new Object[] {version, requiredBlocks, blockSize, entries.size(), dataOffset});
+
+        int i = 0;
+        for(CaptionEntry e : entries) {
+            buf.putInt(e.getKey());
+            buf.putInt(e.getBlock());
+            buf.putShort((short) (e.getOffset() & 0xFFFF));
+            buf.putShort((short) (e.getLength() & 0xFFFF));
+            LOG.log(Level.FINE, "Saving {0}, {1} ({2}->{3})", new Object[] {
+                i++, e.getKey(), e.getOffset(), e.getOffset() + e.getLength()});
+        }
+        buf.put(new byte[dataOffset - buf.position()]);
+        byte[] nul = new byte[2];
+        for(CaptionEntry e : entries) {
+            int p = (dataOffset + (e.getBlock() * blockSize) + e.getOffset());
+            buf.position(p);
+            buf.put(e.getValue().getBytes(VALUE_CHARSET));
+            buf.put(nul);
+        }
+        buf.put(new byte[totalSize - buf.position()]);
+        buf.flip();
+        LOG.log(Level.INFO, "Saved to {0}", buf);
+        return buf;
     }
 
     public static int takeCRC32(String in) {
@@ -220,11 +189,11 @@ public class VCCD {
         ArrayList<String> usedKeys = new ArrayList<String>();
         for(int i = props.size() - 1; i >= 0; i--) {
             Property p = props.get(i);
-//            LOG.log(Level.INFO, "Adding {0}", p);
+            LOG.log(Level.FINER, "Adding {0}", p);
             CaptionEntry e = new CaptionEntry();
             String key = p.getKey().replaceAll("\"", "");
             if(key.equals("//") || key.equals("\\n") || usedKeys.contains(key)) {
-                LOG.log(Level.INFO, "Discarding: {0}", key);
+                LOG.log(Level.WARNING, "Discarding: {0}", key);
                 continue;
             }
             usedKeys.add(key);
@@ -257,9 +226,6 @@ public class VCCD {
         private String trueKey;
 
         public String getTrueKey() {
-//            if(trueKey == null) {
-//                trueKey = attemptDecode((int) key);
-//            }
             return trueKey;
         }
 
@@ -294,7 +260,14 @@ public class VCCD {
             return length;
         }
 
+        /**
+         * Set byte length of value. Has no effect if not a multiple of 2
+         * @param length 
+         */
         public void setLength(int length) {
+            if(length % 2 != 0) {
+                return;
+            }
             this.length = length;
             if(this.value != null) {
                 this.value = value.substring(0, (length / 2) - 1);
@@ -307,17 +280,22 @@ public class VCCD {
             return value;
         }
 
-        public void setValue(String string) {
-            this.value = string;
-            this.length = (string.length() + 1) * 2;
+        public void setValue(String str) {
+            this.value = str;
+            this.length = (str.length() + 1) * 2;
         }
 
         @Override
         public String toString() {
-            return new StringBuilder().append("H: ").append(key).append(", b: ").append(block).append(
-                    ", o: ").append(offset).append(", l: ").append(length).toString();
+            return new StringBuilder()
+                    .append("[H: ").append(key)
+                    .append(", b: ").append(block)
+                    .append(", o: ").append(offset)
+                    .append(", l: ").append(length).append("]")
+                    .append("(").append(trueKey != null ? "'" + trueKey + "'" : "?").append(")")
+                    .append(" = '").append(value).append("'").toString();
         }
-
+        
         public int compareTo(CaptionEntry t) {
             String e1 = this.getTrueKey();
             if(e1 == null) {
---
+++ test/com/timepath/hl2/io/VCCDTest.java
@@ -0,0 +1,52 @@
+package com.timepath.hl2.io;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author TimePath
+ */
+public class VCCDTest {
+    
+    public VCCDTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Tests whether loading and saving produces the same output
+     */
+    @Test
+    public void testLoadSave() throws Exception {
+        File in = new File("/home/timepath/Desktop/in.dat");
+        FileInputStream is = new FileInputStream(in);
+        byte[] array = new byte[is.available()];
+        is.read(array);
+        is.close();
+        ByteBuffer buf = ByteBuffer.wrap(array);
+        ByteBuffer buf2 = VCCD.save(VCCD.load(buf.slice()));
+        assertTrue("content matches", buf.equals(buf2));
+    }
+    
+}
