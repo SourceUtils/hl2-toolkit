20887bdd0bfc9cb888e3214681c73c9d578c2dae
TimePath <andrew.hardaker1995@gmail.com>
2014-01-02 18:25:25 +1100

    CFG lexer

--- src/com/timepath/hl2/io/CFG.java
+++ src/com/timepath/hl2/io/CFG.java
@@ -1,9 +1,10 @@
 package com.timepath.hl2.io;
 
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -13,13 +14,69 @@ public class CFG {
 
     private static final Logger LOG = Logger.getLogger(CFG.class.getName());
 
-    public static void main(String[] args) {
-        readFromString(
-                "alias one \"alias three four\"; alias two // yay\nalias b; alias c alias c alias d alias v taunt");
+    public static enum TokenType {
+
+        COMMENT("//(.*)$"),
+        SPACE("(\\s+)"),
+        SEPARATOR("(;)"),
+        QUOTE("(\\\")"),
+        TOKEN("([^\\s;\\\"]+)");
+
+        public final String pattern;
+
+        private TokenType(String pattern) {
+            this.pattern = pattern;
+        }
+
     }
 
-    public static void readFromString(String s) {
-        parse(new Scanner(s));
+    public static class Token {
+
+        public TokenType type;
+
+        public String data;
+
+        public Token(TokenType type, String data) {
+            this.type = type;
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%s) %s", type.name(), data);
+        }
+
+    }
+
+    private static ArrayList<Token> lex(String input) {
+        ArrayList<Token> tokens = new ArrayList<Token>();
+
+        StringBuilder tokenPatternsBuffer = new StringBuilder();
+        TokenType[] values = TokenType.values();
+        for(TokenType tokenType : values) {
+            tokenPatternsBuffer.append(String.format("|%s", tokenType.pattern));
+        }
+        Pattern tokenPatterns = Pattern.compile(tokenPatternsBuffer.substring(1));
+        Matcher matcher = tokenPatterns.matcher(input);
+        while(matcher.find()) {
+            for(int i = 0; i < values.length; i++) {
+                TokenType type = values[i];
+                String group = matcher.group(i + 1);
+                if(group != null) {
+                    tokens.add(new Token(type, group));
+                    break;
+                }
+            }
+        }
+        return tokens;
+    }
+
+    public static void main(String[] args) {
+        readFromString("alias b; alias c alias c alias d alias v \"taunt; nope\"");
+    }
+
+    public static CFG readFromString(String s) {
+        return CFG.parse(new Scanner(s));
     }
 
     public static void readFromStream(InputStream in) {
@@ -40,58 +97,74 @@ public class CFG {
         }
     }
 
-    private static void parse(Scanner s) {
+    @SuppressWarnings("fallthrough")
+    private static CFG parse(Scanner s) {
+        CFG c = new CFG();
         int lineNum = 0;
         while(s.hasNext()) {
             String line = s.nextLine().trim();
-            char[] pText = line.toCharArray();
             lineNum++;
-            if(pText.length == 0) {
-                continue;
-            }
-
-            int offset = 0;
-            while(offset < pText.length) {
-                int commandLength = getCommand(pText, offset);
-                if(commandLength == 0) {
-                    offset++;
-                    continue;
+            List<Token> q = lex(line);
+            System.out.println(q);
+            Deque<Token> cmd = new LinkedList<Token>();
+            boolean quoted = false;
+            for(Token t : q) {
+                switch(t.type) {
+                    case QUOTE:
+                        quoted = !quoted;
+                    case SEPARATOR:
+                        if(!quoted) {
+                            System.out.println(c.eval(cmd));
+                            cmd.clear();
+                            break;
+                        }
+                    case SPACE:
+                    case TOKEN:
+                        cmd.add(t);
+                        break;
+                    case COMMENT:
+                        break;
+                    default:
+                        throw new AssertionError(t.type.name());
                 }
-                System.out.println(
-                        new String(pText).substring(offset, offset + commandLength).trim());
-                offset += commandLength;
             }
-
+            System.out.println(c.eval(cmd));
         }
+        return c;
     }
 
-    static int getCommand(char[] pText, int offset) {
-        int maxLen = pText.length - offset;
-        boolean quoted = false;
-        boolean commented = false;
-        int i;
-        for(i = 0; i < maxLen; i++) {
-            char c = pText[offset + i];
-            if(commented) {
-                continue;
-            } else if(c == '"') {
-                quoted = !quoted;
-                continue;
-            }
-            if(quoted) {
-                continue;
-            } else if(c == '/') {
-                commented = (i + 1 < maxLen) && pText[offset + i + 1] == '/';
-                if(commented) {
+    private String eval(Deque<Token> l) {
+        StringBuilder sb = new StringBuilder();
+        while(!l.isEmpty()) {
+            Token t = l.pop();
+            switch(t.type) {
+                case COMMENT:
                     break;
-                }
-            }
-            if(c == ';') {
-                break;
+                case SPACE:
+                    break;
+                case SEPARATOR:
+                    break;
+                case QUOTE:
+                    break;
+                case TOKEN:
+                    if(t.data.equals("alias")) {
+                        Alias a = new Alias();
+                        a.name = t.data;
+                        if(!l.isEmpty() && l.peek().type == TokenType.SPACE) {
+                            l.pop();
+                        }
+                        a.cmd = eval(l);
+                        sb.append(a.toString());
+                    } else {
+                        sb.append(t.data);
+                    }
+                    break;
+                default:
+                    sb.append(t.data);
+                    throw new AssertionError(t.type.name());
             }
         }
-
-        return i;
+        return sb.toString();
     }
 
     ArrayList<Alias> aliases = new ArrayList<Alias>();
@@ -106,6 +179,11 @@ public class CFG {
         String cmd;
 
         public Alias() {
+        }
+
+        @Override
+        public String toString() {
+            return "alias " + name + " " + cmd;
         }
 
     }
