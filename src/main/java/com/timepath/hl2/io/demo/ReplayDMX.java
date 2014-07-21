ea9f13bd16a1004bdbaf2c230535f4df78fdcd0c
TimePath <andrew.hardaker1995@gmail.com>
2014-07-21 16:50:13 +1000

    LZSS inflation

--- src/main/java/com/timepath/hl2/io/demo/ReplayDMX.java
+++ src/main/java/com/timepath/hl2/io/demo/ReplayDMX.java
@@ -1,53 +1,68 @@
 package com.timepath.hl2.io.demo;
 
+import com.timepath.hl2.io.demo.LZSS.LZSSException;
 import com.timepath.io.OrderedInputStream;
 import com.timepath.io.struct.StructField;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.math.BigInteger;
 import java.nio.ByteOrder;
 import java.text.MessageFormat;
-import java.util.zip.InflaterInputStream;
 
 /**
+ * for f in $(ls *.block|sort --version-sort); do echo -e "$f\t$(md5sum<$f)  $(wc -c<$f)\t$(bzcat $f|wc -c)"; done
+ * bzcat *.block > joined.dem
+ *
  * @author TimePath
  * @see <a>http://forums.steampowered.com/forums/showthread.php?t=1882941</a>
  */
 public class ReplayDMX {
 
-    public static ReplayDMX load(InputStream is) throws IOException, InstantiationException, IllegalAccessException {
+    public final SessionInfoHeader           info;
+    public final RecordingSessionBlockSpec[] blocks;
+
+    public static ReplayDMX load(InputStream is)
+    throws IOException, InstantiationException, IllegalAccessException, LZSSException
+    {
         return new ReplayDMX(new BufferedInputStream(is));
     }
 
-    ReplayDMX(InputStream is) throws IOException, IllegalAccessException, InstantiationException {
-        System.out.println("header:");
+    public void print(PrintStream out) {
+        out.println("header:");
+        out.println("version: " + info.version);
+        out.println("session name: " + info.sessionName);
+        out.println("currently recording?: " + info.recording);
+        out.println("# blocks: " + info.numBlocks);
+        out.println("compressor: " + CompressorType.get(info.compressorType));
+        out.println("md5 digest: " + md5(info.hash));
+        out.println("payload size (compressed): " + info.payloadSize);
+        out.println("payload size (uncompressed): " + info.payloadSizeUC);
+        out.println("blocks:");
+        out.println("index\tstatus\tMD5\t\t\t\t\t\t\t\t\tcompressor\tsize (uncompressed)\tsize (compressed)");
+        for(int i = 0, blocksLength = blocks.length; i < blocksLength; i++) {
+            RecordingSessionBlockSpec block = blocks[i];
+            out.println(MessageFormat.format("{0}\t\t{1}\t{2}\t{3}\t{4}\t\t{5}\t\t\t\t\t{6}",
+                                                    i,
+                                                    block.reconstruction,
+                                                    block.remoteStatus,
+                                                    md5(block.hash),
+                                                    CompressorType.get(block.compressorType),
+                                                    block.fileSize,
+                                                    block.uncompressedSize));
+        }
+    }
+
+    private ReplayDMX(InputStream is) throws IOException, IllegalAccessException, InstantiationException, LZSSException {
         OrderedInputStream in = new OrderedInputStream(is);
         in.order(ByteOrder.LITTLE_ENDIAN);
-        SessionInfoHeader info = in.readStruct(new SessionInfoHeader());
-        System.out.println("version: " + info.version);
-        System.out.println("session name: " + info.sessionName);
-        System.out.println("currently recording?: " + info.recording);
-        System.out.println("# blocks: " + info.numBlocks);
-        System.out.println("compressor: " + CompressorType.get(info.compressorType));
-        System.out.println("md5 digest: " + md5(info.hash));
-        System.out.println("payload size (compressed): " + info.payloadSize);
-        System.out.println("payload size (uncompressed): " + info.payloadSizeUC);
-        System.out.println("blocks:");
-        System.out.println("index\tstatus\tMD5\t\t\t\t\t\t\t\t\tcompressor\tsize (uncompressed)\tsize (compressed)");
-        in = new OrderedInputStream(new BufferedInputStream(new InflaterInputStream(is))); // TODO: LZSS
+        info = in.readStruct(new SessionInfoHeader());
+        byte[] compressed = new byte[in.available()];
+        in.readFully(compressed);
+        in = new OrderedInputStream(new ByteArrayInputStream(LZSS.inflate(compressed)));
         in.order(ByteOrder.LITTLE_ENDIAN);
+        blocks = new RecordingSessionBlockSpec[info.numBlocks];
         for(int i = 0; i < info.numBlocks; i++) {
-            RecordingSessionBlockSpec spec = in.readStruct(new RecordingSessionBlockSpec());
-            System.out.println(MessageFormat.format("{0}\t\t{1}\t{2}\t{3}\t{4}\t\t{5}\t\t\t\t\t{6}",
-                                                    i,
-                                                    spec.reconstruction,
-                                                    spec.remoteStatus,
-                                                    md5(spec.hash),
-                                                    CompressorType.get(spec.compressorType),
-                                                    spec.fileSize,
-                                                    spec.uncompressedSize));
+            blocks[i] = in.readStruct(new RecordingSessionBlockSpec());
         }
     }
 
@@ -56,7 +71,7 @@ public class ReplayDMX {
         return String.format("%0" + ( hash.length * 2 ) + "x", bi);
     }
 
-    static enum CompressorType {
+    private static enum CompressorType {
         INVALID,
         LZSS,
         BZ2;
@@ -66,7 +81,7 @@ public class ReplayDMX {
         }
     }
 
-    static class SessionInfoHeader {
+    private static class SessionInfoHeader {
 
         static final int MAX_SESSIONNAME_LENGTH = 260;
         @StructField(index = 0)
@@ -89,6 +104,7 @@ public class ReplayDMX {
         /**
          * {@link com.timepath.hl2.io.demo.ReplayDMX.CompressorType.INVALID} if header is not compressed.
          */
+        @SuppressWarnings("JavadocReference")
         @StructField(index = 4)
         int     compressorType;
         /**
