beb5613df7d32f512355938229b89a58ad9befe1
TimePath <andrew.hardaker1995@gmail.com>
2014-09-29 20:12:02 +1000

    Sync with vfs

--- src/main/java/com/timepath/hl2/io/bsp/BSP.java
+++ src/main/java/com/timepath/hl2/io/bsp/BSP.java
@@ -3,19 +3,15 @@ package com.timepath.hl2.io.bsp;
 import com.timepath.hl2.io.bsp.lump.LumpType;
 import com.timepath.io.OrderedInputStream;
 import com.timepath.io.struct.StructField;
-import com.timepath.steam.io.storage.Files;
-import com.timepath.steam.io.storage.Files.FileHandler;
-import com.timepath.vfs.SimpleVFile;
-import com.timepath.vfs.provider.zip.ZipFileProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -33,65 +29,6 @@ public abstract class BSP {
     protected OrderedInputStream in;
     IntBuffer indexBuffer;
     FloatBuffer vertexBuffer;
-
-    static {
-        Files.registerHandler(new FileHandler() {
-            @Nullable
-            @Override
-            public Collection<? extends SimpleVFile> handle(@NotNull final File file) throws IOException {
-                if (!file.getName().endsWith(".bsp")) return null;
-                final String name = file.getName().replace(".bsp", "");
-                return Collections.singleton(new SimpleVFile() {
-                    void checkBSP() {
-                        if (z != null) return;
-                        LOG.log(Level.INFO, "Loading {0}", file);
-                        try (@NotNull InputStream is = new FileInputStream(file)) {
-                            @Nullable BSP b = BSP.load(is);
-                            if (b != null) {
-                                z = b.getLump(LumpType.LUMP_PAKFILE);
-                            }
-                        } catch (IOException e) {
-                            LOG.log(Level.SEVERE, null, e);
-                        }
-                    }
-
-                    @Nullable
-                    ZipFileProvider z;
-
-                    @Override
-                    public boolean isDirectory() {
-                        return true;
-                    }
-
-                    @NotNull
-                    @Override
-                    public String getName() {
-                        return name;
-                    }
-
-                    @Nullable
-                    @Override
-                    public InputStream openStream() {
-                        return null;
-                    }
-
-                    @NotNull
-                    @Override
-                    public Collection<? extends SimpleVFile> list() {
-                        checkBSP();
-                        return z != null ? z.list() : Collections.<SimpleVFile>emptyList();
-                    }
-
-                    @Nullable
-                    @Override
-                    public SimpleVFile get(@NotNull final String name) {
-                        checkBSP();
-                        return z != null ? z.get(name) : null;
-                    }
-                });
-            }
-        });
-    }
 
     BSP() {
     }
@@ -149,8 +86,8 @@ public abstract class BSP {
      *
      * @param <T>
      * @param type
-     * @param handler *
-     * @return *
+     * @param handler
+     * @return
      * @throws IOException
      */
     @Nullable
